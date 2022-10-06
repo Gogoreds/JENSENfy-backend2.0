69f268bb324d10df2141586cc884cdb018ccc54e
Mohammad Arefi <m-arefi@live.com>
2022-10-06 20:22:20 +0200

    Olle fixed the code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 8888;
@@ -7,7 +7,7 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 
-console.log(CLIENT_ID)
+console.log(CLIENT_ID);
 // to test the dotenv
 // console.log(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 
@@ -20,8 +20,10 @@ app.get("/", (req, res) => {
   res.json(data);
 });
 
-app.get('/login', (req, res) => {
-  res.redirect(`https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`);
+app.get("/login", (req, res) => {
+  res.redirect(
+    `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`
+  );
 });
 
 app.listen(port, () => {
